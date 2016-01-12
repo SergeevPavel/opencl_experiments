dfb9be99e3c35e155e845a90461e39b3d25a9301
pavel <sergeev.pavel.al@gmail.com>
2016-01-12 16:15:45 +0300

    fix block size

--- inclusive_scan/main.cpp
+++ inclusive_scan/main.cpp
@@ -91,7 +91,7 @@ void cpu_check(std::vector<float> input, std::vector<float> output)
     }
 }
 
-size_t const block_size = 8;
+size_t const block_size = 64;
 cl::Program program;
 cl::Context context;
 cl::CommandQueue queue;
